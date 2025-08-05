541457f494675851684448575856c78b75f0fe8d
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-05 15:02:44 +0200

    test

--- device.mk
+++ device.mk
@@ -14,8 +14,8 @@ endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
@@ -124,11 +124,11 @@ PRODUCT_PACKAGES += \
 #    checkpoint_gc \
     
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
-    POSTINSTALL_OPTIONAL_system=true
+#AB_OTA_POSTINSTALL_CONFIG += \
+#    RUN_POSTINSTALL_system=true \
+#    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+#    FILESYSTEM_TYPE_system=ext4 \
+#    POSTINSTALL_OPTIONAL_system=true
 
 # AB_OTA_POSTINSTALL_CONFIG += \
 #    RUN_POSTINSTALL_vendor=true \
