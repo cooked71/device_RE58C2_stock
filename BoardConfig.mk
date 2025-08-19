522e186a836da4aee5fa75e7a1dd2db56aafd117
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-19 16:11:52 +0200

    commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,10 +148,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # ========================
 # DTBO/Modules
 # ========================
-BOARD_KERNEL_SEPARATED_DTBO := false
+BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 # Prebuilt DTBO and DT images
-PRODUCT_PREBUILT_DTBO_IMAGE := $(LOCAL_PATH)/prebuilts/dtbo.img
 BOARD_USE_DTBO_IMAGE := true
 
 # Module loading
