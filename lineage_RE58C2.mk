8b50d5080a2017c627b7855e603c42c041b64bdd
cooked71 <53344663+cooked71@users.noreply.github.com>
2025-08-22 15:20:19 +0200

    save

--- lineage_RE58C2.mk
+++ lineage_RE58C2.mk
@@ -14,22 +14,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from RE58C2 device
 $(call inherit-product, device/realme/RE58C2/device.mk)
 
-# ===========================
-# Product identifiers
-# ===========================
+
 PRODUCT_DEVICE := RE58C2
 PRODUCT_NAME := lineage_RE58C2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := ums9230_hulk_Natv
 PRODUCT_MANUFACTURER := realme
-PRODUCT_RELEASE_NAME := RE58C2
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-# ===========================
-# Build properties
-# ===========================
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ums9230_hulk_Natv-user 13 TP1A.220624.014 1063 release-keys" \
-    BUILD_FINGERPRINT=realme/RMX3760/RE58C2:13/TP1A.220624.014/T.R4T2.1720264765:user/release-keys
+    PRIVATE_BUILD_DESC="ums9230_hulk_Natv-user 13 TP1A.220624.014 1063 release-keys"
 
+BUILD_FINGERPRINT := realme/RMX3760/RE58C2:13/TP1A.220624.014/T.R4T2.1720264765:user/release-keys
